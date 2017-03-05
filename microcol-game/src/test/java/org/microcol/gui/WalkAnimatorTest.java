9e065b6979311df8c773f26adc9f73a8edf339b8
jajir <jirout@gmail.com>
2017-03-05 14:07:40 +0100

    test was corrected

--- microcol-game/src/test/java/org/microcol/gui/WalkAnimatorTest.java
+++ microcol-game/src/test/java/org/microcol/gui/WalkAnimatorTest.java
@@ -29,13 +29,13 @@ public class WalkAnimatorTest {
 		/**
 		 * Multiple call should not change result.
 		 */
-		assertEquals(Location.of(32, 62), wa.getNextCoordinates());
-		assertEquals(Location.of(32, 62), wa.getNextCoordinates());
+		assertEquals(Location.of(36, 70), wa.getNextCoordinates());
+		assertEquals(Location.of(36, 70), wa.getNextCoordinates());
 		wa.countNextAnimationLocation();
 
 		assertTrue("first animation step should be available", wa.isNextAnimationLocationAvailable());
-		assertEquals(Location.of(33, 62), wa.getNextCoordinates());
-		assertEquals(Location.of(33, 62), wa.getNextCoordinates());
+		assertEquals(Location.of(37, 70), wa.getNextCoordinates());
+		assertEquals(Location.of(37, 70), wa.getNextCoordinates());
 		wa.countNextAnimationLocation();
 
 		assertEquals("Verify that list size is not changed", 2, path.size());
