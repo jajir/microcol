2b55dfa3957e923f11d583238b1b44bf3598649c
Jiri Kajaba <jiri.kajaba@gmail.com>
2017-03-31 21:11:52 +0200

    Switched logging from java.util.logging to log4j 2.

--- microcol-game/src/main/java/org/microcol/MicroCol.java
+++ microcol-game/src/main/java/org/microcol/MicroCol.java
@@ -3,11 +3,8 @@ package org.microcol;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.SplashScreen;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.logging.LogManager;
 
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -28,21 +25,6 @@ import com.google.inject.Stage;
  * MicroCol's main class.
  */
 public class MicroCol {
-
-	public static class ConsoleHandler extends java.util.logging.ConsoleHandler {
-		@Override
-		protected synchronized void setOutputStream(final OutputStream out) throws SecurityException {
-			super.setOutputStream(System.out);
-		}
-	}
-
-	private static void initJavaLogging() {
-		try {
-			LogManager.getLogManager().readConfiguration(MicroCol.class.getResourceAsStream("/logging.properties"));
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-	}
 
 	/**
 	 * Provide information if it's apple operation system.
@@ -119,8 +101,6 @@ public class MicroCol {
 	 *            the command line arguments
 	 */
 	public static void main(final String... args) {
-
-		initJavaLogging();
 
 		setSplashScreen();
 
