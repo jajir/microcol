69b3b25f05e80de6d7072472cff49b0b5d7d62f9
Jiri Kajaba <jiri.kajaba@gmail.com>
2017-04-09 08:18:55 +0200

    #25 Started implementation of save/load.

--- microcol-game/src/test/java/org/microcol/model/ModelSaveLoadTest.java
+++ microcol-game/src/test/java/org/microcol/model/ModelSaveLoadTest.java
@@ -1,13 +1,14 @@
 package org.microcol.model;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class ModelSerializationTest {
+public class ModelSaveLoadTest {
 	@Test
-	public void serializeTest() {
+	public void saveTest() {
 		/*
 		final ModelBuilder builder = new ModelBuilder();
 		final Model model = builder
@@ -21,9 +22,21 @@ public class ModelSerializationTest {
 				.addShip("Player2", ShipType.FRIGATE, Location.of(7, 9))
 				.addShip("Player2", ShipType.FRIGATE, Location.of(14, 9))
 			.build();
+		model.startGame();
+		final Ship ship = model.getCurrentPlayer().getShips().get(0);
 		*/
-		final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		final String result = gson.toJson(Location.of(1, 1));
-		System.out.println(result);
+
+		final Gson gson = new GsonBuilder().create();
+		final String json = gson.toJson(Location.of(1, 1));
+
+		Assert.assertEquals("{\"x\":1,\"y\":1}", json);
+	}
+
+	@Test
+	public void loadTest() {
+		final Gson gson = new GsonBuilder().create();
+		final Location location = gson.fromJson("{\"x\": 1,\"y\": 1}", Location.class);
+
+		Assert.assertEquals(Location.of(1, 1), location);
 	}
 }
