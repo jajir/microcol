6676cfe2439eb417c9ec340bb283b074c2747ea5
jajir <jirout@gmail.com>
2017-05-08 20:58:22 +0200

    dock icon is used on mac

--- microcol-game/src/main/java/org/microcol/MicroCol.java
+++ microcol-game/src/main/java/org/microcol/MicroCol.java
@@ -1,5 +1,12 @@
 package org.microcol;
 
+import java.awt.Image;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.imageio.ImageIO;
+
 import org.microcol.gui.ApplicationController;
 import org.microcol.gui.MainStageBuilder;
 import org.microcol.gui.MicroColModule;
@@ -16,8 +23,48 @@ public class MicroCol extends javafx.application.Application {
 		launch(args);
 	}
 
+	/**
+	 * Method try to set application icon. It use apple native classes. Because
+	 * it should be compiled at windows platform apple specific classes are
+	 * access by java reflection. For this reason is also exception sunk.
+	 */
+	private static void setAppleDockIcon() {
+		try {
+			Class<?> clazz = Class.forName("com.apple.eawt.Application", false, null);
+			if (clazz != null) {
+				Method m = clazz.getMethod("getApplication");
+				Object o = m.invoke(null);
+				Method m2 = clazz.getMethod("setDockIconImage", Image.class);
+				final ClassLoader cl = MicroCol.class.getClassLoader();
+				final Image i = ImageIO.read(cl.getResourceAsStream("images/splash-screen.png"));
+				m2.invoke(o, i);
+			}
+		} catch (IOException | ClassNotFoundException | NoSuchMethodException | SecurityException
+				| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			/**
+			 * intentionally do nothing.
+			 */
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Provide information if it's apple operation system.
+	 * 
+	 * @return return <code>true</code> when it's apple operation system
+	 *         otherwise return <code>false</code>
+	 */
+	private static boolean isOSX() {
+		final String osName = System.getProperty("os.name");
+		return osName.contains("OS X");
+	}
+
 	@Override
 	public void start(final Stage primaryStage) throws Exception {
+		if (isOSX()) {
+			setAppleDockIcon();
+		}
+
 		Platform.runLater(() -> {
 			try {
 				final Injector injector = Guice.createInjector(com.google.inject.Stage.PRODUCTION,
