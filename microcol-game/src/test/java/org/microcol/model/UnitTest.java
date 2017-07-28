668473c55006252c40b2aaace66ac3ed2c5e26ac
jajir <jirout@gmail.com>
2017-07-28 21:53:43 +0200

    pokus

--- microcol-game/src/test/java/org/microcol/model/UnitTest.java
+++ microcol-game/src/test/java/org/microcol/model/UnitTest.java
@@ -2,7 +2,10 @@ package org.microcol.model;
 
 import org.junit.Test;
 
- import static org.junit.Assert.*;
+import static org.junit.Assert.*;
+
+import mockit.Injectable;
+import mockit.Mocked;
 import mockit.Tested;
 import mockit.Verifications;
 
@@ -10,15 +13,26 @@ public class UnitTest {
 
 	@Tested	Unit unit;
 
+	@Injectable UnitType tyoe = UnitType.COLONIST;
+	
+	@Injectable Player owner;
+	
+	@Injectable Location location = Location.of(2, 5);
+	
 	@Test
 	public void testGetHold() {
 		assertNotNull(unit);
 		CargoHold cargo = unit.getHold();
-		new Verifications() {
-			{
-				times = 1;
-			}
-		};
+		
+		assertNotNull(cargo);
 	}
+	
+	@Test
+	public void testIsPossible(@Mocked Model model) {
+		unit.setModel(model);
+		assertEquals(true, unit.isMoveable(Location.of(12, 4)));
+	}
+	
+	
 
 }
