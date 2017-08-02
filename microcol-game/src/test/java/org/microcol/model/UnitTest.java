f824550c850da8daf19cadbc849659725de66e5c
Jirout Jan <Jan.Jirout@equabank.cz>
2017-08-02 18:04:45 +0200

    jmockit was added

--- microcol-game/src/test/java/org/microcol/model/UnitTest.java
+++ microcol-game/src/test/java/org/microcol/model/UnitTest.java
@@ -2,23 +2,55 @@ package org.microcol.model;
 
 import org.junit.Test;
 
- import static org.junit.Assert.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.Mocked;
 import mockit.Tested;
-import mockit.Verifications;
 
 public class UnitTest {
 
-	@Tested	Unit unit;
+	@Tested(availableDuringSetup=true)
+	Unit unit;
 
+	@Injectable
+	UnitType type = UnitType.COLONIST;
+
+	@Injectable
+	Player owner;
+
+	@Injectable()
+	Location location = Location.of(4, 3);
+
+	@Mocked Model model;
+	
 	@Test
-	public void testGetHold() {
+	public void testInitialization() {
 		assertNotNull(unit);
-		CargoHold cargo = unit.getHold();
-		new Verifications() {
-			{
-				times = 1;
-			}
-		};
+		assertEquals(Location.of(4, 3), unit.getLocation());
+		assertEquals(owner, unit.getOwner());
 	}
-
+	
+	@Test
+	public void testIsMovable() throws Exception {
+		final Location loc =Location.of(10, 12);
+		new Expectations() {{
+			model.getMap().isValid(loc); result=true;
+			model.getMap().getTerrainAt(loc); result=Terrain.CONTINENT;
+		}};
+		assertTrue(unit.isMoveable(loc));
+	}
+	
+	@Before
+	public void setup() {
+		new Expectations() {{
+			model.getMap().isValid(location); result=true;
+			model.getMap().getTerrainAt(location); result=Terrain.CONTINENT;
+		}};
+		unit.setModel(model);
+	}
+	
 }
