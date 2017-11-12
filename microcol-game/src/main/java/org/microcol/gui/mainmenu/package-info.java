5c9cecd65d0a26c86e349deae2f0815736e25e2e
jajir <jirout@gmail.com>
2017-11-12 20:16:39 +0100

    doc was added

---
+++ microcol-game/src/main/java/org/microcol/gui/mainmenu/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * Contains main menu presentation and all controllers that register events
+ * raised by main menu.
+ * <ul>
+ * <li>{@link org.microcol.gui.mainmenu.AboutGameEventController}</li>
+ * <li>{@link org.microcol.gui.mainmenu.AnimationSpeedChangeController}</li>
+ * <li>{@link org.microcol.gui.mainmenu.BuildColonyEventController}</li>
+ * <li>{@link org.microcol.gui.mainmenu.CenterViewController}</li>
+ * <li>{@link org.microcol.gui.mainmenu.ChangeLanguageController}</li>
+ * <li>{@link org.microcol.gui.mainmenu.DeclareIndependenceController}</li>
+ * <li>{@link org.microcol.gui.mainmenu.ExitGameController}</li>
+ * <li>{@link org.microcol.gui.mainmenu.ShowGridController}</li>
+ * <li>{@link org.microcol.gui.mainmenu.VolumeChangeController}</li>
+ * </ul>
+ * 
+ * @since 0.4
+ */
+package org.microcol.gui.mainmenu;
